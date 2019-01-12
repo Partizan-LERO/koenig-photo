a55a543f8073865e4a0e086fc326b1c92348d1d1
Sergey Samoylenko <s.samoylenko@invitedriver.com>
2019-01-12 18:28:11 +0200

    add webmaster

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Фотосессии Калининград, фотограф, не дорого, с животными, прогулочная, свадебная, lovestory, праздничная, студийная, детская" name="keywords">
     <meta content="Профессиональные фотосессии в Калининграде. Фотосессии с животными. LoveStory. Свадебные фотосесии" name="description">
+    <meta name="yandex-verification" content="29398ac98a339293" />
 
     <!-- Favicons -->
     <link href="img/icons/favicons/favicon.png" rel="icon">
@@ -60,6 +61,21 @@
 
 <!-- Template Main Javascript File -->
 <script src="/js/main.js"></script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+    ym(51885074, "init", {
+        id:51885074,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/51885074" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </body>
 </html>
